2892b1a9c8585423b9b0af336f3fbe0dce991ae6
matias725 <alexisbordones18@gmail.com>
2025-11-06 15:41:04 +0100

    Add gunicorn to requirements

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -21,6 +21,11 @@ DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
+# Production settings
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
